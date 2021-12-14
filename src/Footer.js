a50f7689360adb1e9d65501be7175ffdda2262f5
+ <+>
2021-12-14 08:51:17 -0500

    add footer

---
+++ src/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h5>Contact Info: </h5>
+
+      <a className="bloglinks" href="mailto:spfohman@email.essex.edu">
+        Sarah pfohman
+      </a>
+      <br />
+      <a className="bloglinks" href="https://github.com/spfohman">
+        Github
+      </a>
+      <br />
+      <a
+        className="bloglinks"
+        href="https://www.linkedin.com/in/sarah-pfohman-998a8246/"
+      >
+        LinkedIn
+      </a>
+    </div>
+  );
+};
+export default Footer;
--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,44 @@
 import React, { useState, useEffect } from "react";
+import Footer from "./Footer";
 
 const Home = () => {
   const [cat, setCat] = useState([]);
+  const [displayFooter, setDisplayFooter] = useState(false);
+
   useEffect(() => {
     fetch("https://api.thecatapi.com/v1/images/search")
       .then((response) => response.json())
       .then((data) => setCat(data));
   }, []);
 
+  const showMore = (event) => {
+    event.preventDefault();
+    console.log("button click");
+    setDisplayFooter(true);
+  };
+
   const eachCat = cat.map((cat) => {
     return <img className="image" src={cat.url} alt={cat.id} key={cat.id} />;
   });
 
   return (
-    <div>
-      <h1>Welcome to my home page</h1>
-      <p>
-        My name is Sarah Pfohman, I am a current student at Flatiron School,
-        based in NYC! I love cats, true crime, and learning to code!
-      </p>
-      {eachCat}
-    </div>
+    <>
+      <div>
+        <h1>Welcome to my home page</h1>
+        <p>
+          My name is Sarah Pfohman, I am a current student at Flatiron School,
+          based in NYC! I love cats, true crime, and learning to code!
+        </p>
+        {eachCat}
+        <br />
+      </div>
+      <footer>
+        <button className="button" onClick={showMore}>
+          Show more
+        </button>
+        {displayFooter ? <Footer /> : null}
+      </footer>
+    </>
   );
 };
 export default Home;
